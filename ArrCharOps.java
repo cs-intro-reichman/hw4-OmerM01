3950c527cc04d9674b455fb7ebe42aed66e898f3
OmerM01 <omerwork00@gmail.com>
2024-11-28 09:03:26 +0200

    HW_02

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -171,7 +171,43 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        // Replace the following statement with your code
-        return 0;
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        boolean isLexi = true;
+        if(str1 == str2) return 0; // what about capitals?
+        if(str1.length() < str2.length()){
+            for(int i = 0; i < str1.length(); i++){
+                if(str1.charAt(i) != str2.charAt(i)){
+                    isLexi = false;
+                }
+            }
+        }
+        if(isLexi){
+            return -1;
+        }
+
+        if(str1.length() < str2.length()){
+            for(int i = 0; i < str1.length(); i++){
+                if(str1.charAt(i) < str2.charAt(i)){
+                    return -1;
+                }
+            }
+        }
+
+        if(str1.length() == str2.length() && str1 != str2){
+            for(int i = 0; i < str1.length(); i++){
+                if(str1.charAt(i) > str2.charAt(i)){
+                    return 1;
+                }
+                else if(str1.charAt(i) < str2.charAt(i)){
+                    return -1;
+                }
+            }
+        }
+
+        
+       else{
+        return -2;
+       }
     }
 }
