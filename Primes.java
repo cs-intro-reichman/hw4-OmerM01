7f21649a5a35a98fe00418624128472d55401ac3
OmerM01 <omerwork00@gmail.com>
2024-12-02 12:15:14 +0200

    HW_26_n

--- Primes.java
+++ Primes.java
@@ -1,5 +1,29 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int primesCount = 0;
+        int n = Integer.parseInt(args[0]);
+        boolean[] isPrime = new boolean[n + 1];
+
+        for(int i = 2; i < isPrime.length; i++){ // setting the arr
+            isPrime[i] = true;
+        }
+
+        int p =2;
+        while(p <= Math.sqrt(n)){ //marking all non-prime nums
+            for(int i = p + 1; i < isPrime.length; i++){
+                if(i % p == 0){
+                    isPrime[i] = false;
+                }
+            }
+            p++;
+        }
+
+        for(int i = 2; i < isPrime.length; i++){ //counting all prime nums
+            if(isPrime[i] == true){
+                primesCount++;
+            }
+        }
+        int precentage = (primesCount * 100) / n;
+        System.out.println("There are " + primesCount + " between 2 and " + n + " (" + precentage + "% are primes)");
     }
 }
