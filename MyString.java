570c74ff64e0ec210ba034c9744a869278200cf5
OmerM01 <omerwork00@gmail.com>
2024-12-02 14:50:13 +0200

    HW_37

--- MyString.java
+++ MyString.java
@@ -34,7 +34,25 @@ public class MyString {
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        
+        if (str1 == null || str2 == null) {
+            return false;
+        }
+        if (str2.isEmpty()) {
+            return true;
+        }
+
+        for (int i =0; i <= str1.length() - str2.length(); i++) {
+            boolean found = true;
+            for (int j = 0; j < str2.length(); j++) {
+                if (str1.charAt(i + j) != str2.charAt(j)) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                return true;
+            }
+        }
         return false;
     }
 }
